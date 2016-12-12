77d1e9abcbc2e3e9515a69838ba0e34cb496350b
Max Jacobson <max@hardscrabble.net>
2016-12-11 20:01:16 -0500

    day 5 part 2
    this one is even slower! like 4+ minutes!

--- day-05/src/password.rs
+++ day-05/src/password.rs
@@ -1,6 +1,7 @@
 extern crate crypto;
 use self::crypto::digest::Digest;
 use self::crypto::md5::Md5;
+use std::collections::HashMap;
 
 pub struct Password {
     door_id: String,
@@ -12,16 +13,33 @@ impl Password {
     }
 
     pub fn val(&self) -> String {
-        (0..)
-            .filter_map(|n| {
-                let hash = self.hashed_counter(n);
-                match hash.starts_with("00000") {
-                    true => Some(hash.chars().nth(5).unwrap()),
-                    false => None,
+        let mut result: HashMap<u32, char> = HashMap::new();
+        let mut counter = 0;
+        loop {
+            if result.len() == 8 {
+                let mut more_result: Vec<(&u32, &char)> = result.iter().collect();
+                more_result.sort_by(|a, b| a.0.cmp(b.0));
+                let final_result: String = more_result.iter().map(|r| *r.1).collect();
+                println!("{:?}", more_result);
+                return final_result;
+            }
+
+
+            let hash = self.hashed_counter(counter);
+            if hash.starts_with("00000") {
+                match hash.chars().nth(5).unwrap().to_string().parse::<u32>() {
+                    Ok(pos) => {
+                        if pos < 8 {
+                            let val = hash.chars().nth(6).unwrap();
+                            result.entry(pos).or_insert(val);
+                        }
+                    }
+                    Err(_) => {}
                 }
-            })
-            .take(8)
-            .collect()
+            }
+
+            counter += 1;
+        }
     }
 
     fn hashed_counter(&self, counter: u32) -> String {
